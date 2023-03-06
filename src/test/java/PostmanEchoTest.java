77966995ae9f00126dfb0af2090eb5897b7fcb73
Alex Kir <alexkir800@gmail.com>
2023-03-06 15:13:34 +0200

    first commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void test() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(/* -- > Ваша проверка здесь <-- */)
+        ;
+    }
+}
